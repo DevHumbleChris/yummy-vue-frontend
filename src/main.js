5486bbdae4e743356b9c86a45cc13fb8539132ec
DevHumbleChris <dev.humblechris254@gmail.com>
2022-02-02 19:37:18 +0300

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -3,5 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './index.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons/faAddressBook'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(fas, fab, far)
+
+createApp(App).use(store).use(router).component('FontAwesomeIcon', FontAwesomeIcon).mount('#app')

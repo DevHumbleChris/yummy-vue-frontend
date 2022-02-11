52d8d6770b01977be94aa717fcae018a2ecf503d
DevHumbleChris <dev.humblechris254@gmail.com>
2022-02-11 20:49:05 +0300

    succesfully connected vue and django backend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="home">
     <HelloWorld />
+    <div>{{ responseData }}</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from 'axios'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data () {
+    return {
+      responseData: ''
+    }
+  },
   components: {
     HelloWorld
+  },
+  mounted () {
+    const response = axios.get('http://127.0.0.1:800/api/v1/all-products/')
+    this.responseData = response.data
   }
 }
 </script>

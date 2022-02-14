78787a95a841e0db2d280b0ca5b3fce318cfc675
DevHumbleChris <dev.humblechris254@gmail.com>
2022-02-14 13:54:00 +0300

    changed the store baseURL

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,5 @@
 export default {
-  baseURL: 'http://127.0.0.1:8000/api/v1/',
+  baseURL: 'https://e-yummy-ke-api.herokuapp.com/api/v1/',
   all_products: [],
   isAuthenticated: false,
   categories: []
